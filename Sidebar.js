d3886e341a9a1972e7b76785ae9b5b2a9ea8a3b9
kidvalor <87155806+kidvalor@users.noreply.github.com>
2021-08-25 20:47:52 -0400

    Initial commit

---
+++ Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//we create our class Component
+class Sidebar extends React.Component {
+    render() {
+        return(
+            <div id="header">React Tac Toe</div>
+        )
+    }
+}
+
+export default Sidebar
---
+++ app.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+
+class App extends React.Component {
+    // class Animal 
+    // class Pet extends class Animal
+  
+  
+    render () {
+      return(
+        <div id ="app">
+          <Sidebar />
+        </div>
+      )
+    }
+  }
+  export default App;
